497e06d48edf8fd4d03c76afdedacd6644e17bab
krzywon <krzywon@yahoo.com>
2017-10-19 10:52:39 -0400

    Remove GUI necessity and add method to remove temp files when finished.

--- test/projectsaveandload/test/utest_project.py
+++ test/projectsaveandload/test/utest_project.py
@@ -5,22 +5,22 @@
 import os
 import sys
 import unittest
-import logging
 import warnings
 if sys.version_info[0] >= 3:
     from io import StringIO
 else:
     from StringIO import StringIO
 
-from sas.sasview.sasview import SasView
 from sas.sascalc.dataloader.loader import Loader
-from sas.sascalc.fit.pagestate import PageState
-from sas.sasgui.perspectives.fitting.fitpage import FitPage
+from sas.sascalc.fit.pagestate import Reader as fit_state_reader
+from sas.sasgui.perspectives.invariant.invariant_state import Reader as invariant_reader
+from sas.sasgui.perspectives.pr.inversion_state import Reader as pr_reader
+from sas.sasgui.perspectives.corfunc.corfunc_state import Reader as corfunc_reader
 
-logger = logging.getLogger(__name__)
 warnings.simplefilter("ignore")
 
 TEMP_FOLDER = "temp_folder"
+STATE_LOADERS = [fit_state_reader, invariant_reader, pr_reader, corfunc_reader]
 
 
 class projects(unittest.TestCase):
@@ -29,28 +29,22 @@ class projects(unittest.TestCase):
         """
         Set up the base unit test class and variables used throughout the tests
         """
+        self.addCleanup(self.remove_dir)
         self.loader = Loader()
         self.data1d = self.loader.load("test_data/data1D.h5")
         self.data2d = self.loader.load("test_data/data2D.h5")
         if not (os.path.isdir(TEMP_FOLDER)):
             os.makedirs(TEMP_FOLDER)
-        self.gui = SasView()
 
-    def addCleanUp(self):
-        """
-        Close any open files, close any open GUI elements, and remove temp files
-        """
+    def remove_dir(self):
         if(os.path.isdir(TEMP_FOLDER)):
             os.removedirs(TEMP_FOLDER)
-        self.gui.gui.Close()
 
     def test_saveload_data1d_fitting_only(self):
         """
         Test saving and loading a project with a single data set sent to fitting
         """
-        #fitpage.fill_data_combobox([self.data1d])
-        #fitpage.categorybox.SetSelection(4)
-        #fitpage.formfactorbox.SetSelection(6)
+        self.assertTrue(1 == 1)
 
         # TODO: Send 1D to fitting, select model, save project, load project
         # TODO: Send both to fitting, select model on both, save/load
