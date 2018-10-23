08f921e4b9541718a91364df07b9c20f80ac7ce8
krzywon <krzywon@yahoo.com>
2018-10-23 07:49:09 -0400

    Thread sasview wx gui so it can be called and run in parallel with tests.

--- test/projectsaveandload/test/utest_project.py
+++ test/projectsaveandload/test/utest_project.py
@@ -6,10 +6,7 @@ import os
 import sys
 import unittest
 import warnings
-if sys.version_info[0] >= 3:
-    from io import StringIO
-else:
-    from StringIO import StringIO
+import threading
 
 from sas.sascalc.dataloader.loader import Loader
 from sas.sascalc.fit.pagestate import Reader as fit_state_reader
@@ -33,8 +30,18 @@ class projects(unittest.TestCase):
         self.loader = Loader()
         self.data1d = self.loader.load("test_data/data1D.h5")
         self.data2d = self.loader.load("test_data/data2D.h5")
+        self.sasviewThread = sasviewThread()
+        self.sasviewThread.start_local()
         if not (os.path.isdir(TEMP_FOLDER)):
             os.makedirs(TEMP_FOLDER)
+
+    def tearDown(self):
+        self.remove_dir()
+        if hasattr(self.sasviewThread, "isAlive"):
+            if self.sasviewThread.isAlive():
+                print("TODO: Close app directly")
+                self.app.gui.Close()
+                pass
 
     def remove_dir(self):
         if(os.path.isdir(TEMP_FOLDER)):
@@ -44,6 +51,7 @@ class projects(unittest.TestCase):
         """
         Test saving and loading a project with a single data set sent to fitting
         """
+        self.sasviewThread.join(5)
         self.assertTrue(1 == 1)
 
         # TODO: Send 1D to fitting, select model, save project, load project
@@ -52,3 +60,35 @@ class projects(unittest.TestCase):
         # TODO: Send 1D to each other persepective, save/load
         # TODO: Send 1D to every perspective, save/load
         # TODO: Save/load simultaneous/constrained fit project
+
+
+class sasviewThread(threading.Thread):
+    """Run the MainLoop as a thread. Access the frame with self.frame."""
+
+    def __init__(self, autoStart=True):
+        threading.Thread.__init__(self)
+        self.setDaemon(1)
+        self.start_orig = self.start
+        self.start = self.start_local
+        self.frame = None  # to be defined in self.run
+        self.lock = threading.Lock()
+        self.lock.acquire()  # lock until variables are set
+        if autoStart:
+            self.start()  # automatically start thread on init
+
+    def run(self):
+        import sas.sasview.sasview as sasview
+        app = sasview.run_gui()
+        self.frame = app.frame
+
+        # define frame and release lock
+        # The lock is used to make sure that SetData is defined.
+        self.lock.release()
+
+        app.MainLoop()
+
+    def start_local(self):
+        self.start_orig()
+        # After thread has started, wait until the lock is released
+        # before returning so that functions get defined.
+        self.lock.acquire()
